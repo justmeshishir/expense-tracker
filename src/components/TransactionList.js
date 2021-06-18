01292a7ee1a6228dce3b1141cfdd89fd990ee72d
Shishir Thapa <shishirthapa1@gmail.com>
2021-06-18 09:03:52 +0545

    hide history if length is 0

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -7,12 +7,16 @@ export const TransactionList = () => {
     
     return (
         <>
-            <h3>History</h3>
-            <ul className="list">
-                {transactions.map(transaction => (
-                    <Transaction key={transaction.id} transaction={transaction}/>
-                ))}
-            </ul>
+        { transactions.length > 0 && 
+            <div>
+                <h3>History</h3>
+                <ul className="list">
+                    {transactions.map(transaction => (
+                        <Transaction key={transaction.id} transaction={transaction}/>
+                    ))}
+                </ul>
+            </div>
+        }
         </>
     )
 }
